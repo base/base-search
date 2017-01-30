51ee64a7ed041cf83060b521586fbba3ddc6f366
doowb <brian.woodward@gmail.com>
2017-01-30 16:13:25 -0500

    add code comments for readme

--- index.js
+++ index.js
@@ -3,10 +3,23 @@
 var Search = require('./lib/search');
 var utils = require('./lib/utils');
 
-module.exports = function(config) {
-  return function(app) {
-    if (!utils.isValid(app, 'base-search')) return;
+/**
+ * Plugin for [base][] applications like [generate][], [assemble][], [verb][], and [update][]
+ * to add methods for creating search indexices using [indexers](#indexers).
+ *
+ * ```js
+ * var app = assemble();
+ * app.use(search());
+ * ```
+ * @name search
+ * @param  {Object} `config` Configuration object used to specify default indexer to use.
+ * @return {Function} Plugin function passed to `app.use` methods.
+ * @api public
+ */
 
+module.exports = function(config) {
+  return function plugin(app) {
+    if (!utils.isValid(app, 'base-search')) return;
     app.define('search', new Search(config));
   };
 };
